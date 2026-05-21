31f388f4d44e00d09fee14f607421febef998818
Solia Mark <soliamark25@gmail.com>
2026-05-21 16:39:12 +0300

    chore(eslint): configure ESLint, Stylelint, Prettier import sorting SCRUM-22

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,7 +1,34 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from "eslint-plugin-storybook";
-
+import importPlugin from "eslint-plugin-import";
 import { nextJsConfig } from "@repo/eslint-config/next-js";
 
 /** @type {import("eslint").Linter.Config[]} */
-export default nextJsConfig;
+export default [
+  ...nextJsConfig,
+  ...storybook.configs["flat/recommended"],
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      // No console.log in production code
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+
+      // No unused variables
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+
+      // Import rules
+      "import/no-unresolved": "error",
+      "import/no-unused-modules": "warn",
+      "import/first": "error",
+      "import/newline-after-import": "error",
+    },
+  },
+];
