04bc038217ab25876167ba4d8ce4e6d238a06f74
Solia Mark <soliamark25@gmail.com>
2026-07-31 00:32:44 +0300

    refactor(api): split RepertoireController into UserRepertoire and BandRepertoire controllers

--- apps/api/src/repertoire/repertoire.module.ts
+++ apps/api/src/repertoire/repertoire.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { RepertoireController } from './repertoire.controller';
+import { BandRepertoireController } from './controllers/band-repertoire.controller';
 import { RepertoireService } from './repertoire.service';
+import { UserRepertoireController } from './controllers/user-repertoire.controller';
 
 @Module({
-  controllers: [RepertoireController],
+  controllers: [UserRepertoireController, BandRepertoireController],
   providers: [RepertoireService],
   exports: [RepertoireService],
 })
